3c1e757b3fb7fa198d9252c898cc8a75baaaed68
CAPITAL <CAPITAL@DESKTOP-Q1RT2NB>
2023-08-31 10:06:06 -0700

    Add project files.

---
+++ LeaveManagerTest3/Controllers/AccountController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using LeaveManagerTest3.Models;
+
+namespace LeaveManagerTest3.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Models.Identity model)
+        {
+            using (var context = new LeaveSystemDatabaseEntities1())
+            {
+                bool isValid =context.User.Any(x=>x.Username==model.Username && x.Password==model.password);
+                if (isValid)
+                {
+                    FormsAuthentication.SetAuthCookie(model.Username, false);
+                    return RedirectToAction("Index","Employees");
+                }
+                ModelState.AddModelError("","Invalid credentials");
+                return View();
+            }
+           
+        }
+
+        public ActionResult Signup()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Signup(User model)
+        {
+            using(var context = new LeaveSystemDatabaseEntities1())
+            {
+                context.User.Add(model);
+                context.SaveChanges();  
+            }
+            return RedirectToAction("Login");
+            
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+
+    }
+}
---
+++ LeaveManagerTest3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LeaveManagerTest3.MvcApplication" Language="C#" %>
